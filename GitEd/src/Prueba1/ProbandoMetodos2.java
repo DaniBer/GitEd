4c32430ddf23e2da81145a60bc0e60d58bf05843
alumno <alumno@alumno-desktop>
2015-04-23 13:29:02 +0200

    ojala

--- GitEd/src/Prueba1/ProbandoMetodos2.java
+++ GitEd/src/Prueba1/ProbandoMetodos2.java
@@ -4,8 +4,11 @@ public class ProbandoMetodos2 {
 	public void jesuz(){
 		System.out.println("Esto es una risa");
 	}
+
+	public void jesuzchrist(){
+		System.out.println("Fusión ya!!!");
+	}
 	public void jesuz2(){
 		System.out.println("Esto es una risa");
 	}
-	
 }
